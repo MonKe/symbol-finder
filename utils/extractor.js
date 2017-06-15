c629f6399fc0ae5a2636ef08578409e9450a95e5
K. Thommy <kevin.thommy@gmail.com>
2017-06-15 12:27:38 +0200

    made the data flat and refactored the extractor

--- utils/extractor.js
+++ utils/extractor.js
@@ -16,6 +16,7 @@ const
 
 let trimmedData = []
 
+console.log(`Reading from ${DIR}/*`)
 fs.readdir(DIR, function(err, items) {
   items.forEach(file => {
     fs.readFile(path.join(DIR, file), "utf8", (error, data) => {
@@ -26,23 +27,20 @@ fs.readdir(DIR, function(err, items) {
           ucdBlock.group.char.map(char =>
             Char(
               char['-cp'],
-              Object.keys(char)
-                .filter(key => /^\-na/.test(key))
-                .map(key => char[key])
+              matchKeys(char, /^\-na/)
             )
           )
         )
-      console.log(trimmedBlock.blk, trimmedBlock.char.length)
+
+      console.log(trimmedBlock.blk, trimmedBlock.char.length, 'chars')
       trimmedData.push(trimmedBlock)
 
-      // afterwards
+      // when all is trimmed
       if (trimmedData.length === items.length) {
-        if (FLAG_FLAT)
-          trimmedData = trimmedData
-            .reduce((data, group) => [...data, ...group.char], [])
+        console.log(`Writing to ${OUT}`)
         fs.writeFile(
           OUT,
-          JSON.stringify({data: trimmedData}, null, 2),
+          JSON.stringify(Data(trimmedData), null, 2),
           e => e ? console.error(e) : null
         )
       }
@@ -52,5 +50,11 @@ fs.readdir(DIR, function(err, items) {
 
 // lib
 
-const Block = (blk, char) => ({blk, char})
-const Char = (cp, na) => ({cp, na})
+const
+  Data = obj => ({data: FLAG_FLAT ? flatten(obj) : obj}),
+  Block = (blk, char) => ({blk, char}),
+  Char = (cp, na) => ({cp, na}),
+
+  flatten = obj => obj.reduce((data, group) => [...data, ...group.char], []),
+  matchKeys = (obj, regexp) =>
+    Object.keys(obj).filter(key => regexp.test(key)).map(key => obj[key])
